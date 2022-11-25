9dd3d39ebaa81e4f54d948b3d98f7e25bed0f679
Hailegebriel22 <eartalea@gmail.com>
2022-11-25 20:57:03 +0100

    fix build fail

--- packages/client/src/components/body/body.component.js
+++ packages/client/src/components/body/body.component.js
@@ -9,24 +9,46 @@ import Closet from '../../assets/closet.png';
 
 export const Body = () => {
   return (
-    <main className='main'>
-      <span className='shop-our-collections'>Shop Our collections</span>
+    <main className="main">
+      <span className="shop-our-collections">Shop Our collections</span>
       <div className="home-main-img-container">
         <div className="upper-img-container">
-          <img className="img-women-accessories" src={WomenAccessories} alt="earring and shoe" />
+          <img
+            className="img-women-accessories"
+            src={WomenAccessories}
+            alt="earring and shoe"
+          />
           <div className="upper-right-container">
             <img
-              className="img-men-accessories" src={MenAccessories}
-              alt="men shoe and clothes" />
-            <img className="img-handbag" src={Handbag} alt="a person holding a handbag" />
+              className="img-men-accessories"
+              src={MenAccessories}
+              alt="men shoe and clothes"
+            />
+            <img
+              className="img-handbag"
+              src={Handbag}
+              alt="a person holding a handbag"
+            />
           </div>
         </div>
-        <div className='lower-img-container'>
-          <div className='lower-left-container'>
-            <img className="img-woman-glass" src={womanGlasses} alt="a woman in glasses" />
-            <img className="img-pearl-earring" src={PearlEarring} alt="pearl earring" />
+        <div className="lower-img-container">
+          <div className="lower-left-container">
+            <img
+              className="img-woman-glass"
+              src={womanGlasses}
+              alt="a woman in glasses"
+            />
+            <img
+              className="img-pearl-earring"
+              src={PearlEarring}
+              alt="pearl earring"
+            />
           </div>
-          <img className="img-closet" src={Closet} alt="big wardrobe closet with clothes hanging" />
+          <img
+            className="img-closet"
+            src={Closet}
+            alt="big wardrobe closet with clothes hanging"
+          />
         </div>
       </div>
       <button className="shopnow-button" type="button">
