b24a821feb2bf48031c4881f614a67e99c27d397
HackYourFutureDK <karolina.urnieziute@gmail.com>
2022-11-27 15:30:04 +0200

    fix(LandingPage): cleanup

--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -5,23 +5,12 @@ import { Header } from '../../components/Header/Header.component';
 import Footer from '../../components/Footer/Footer';
 import ProductList from '../../components/ProductList/ProductList.component';
 
-
 export const LandingPage = () => {
   return (
-    <>
-      <div className="landing-page-container">
-        <span>Landing Page</span>
-        {exampleResources.map((example) => (
-          <div key={example.id}>{example.title}</div>
-        ))}
-      </div>
-      <Newsletter />
-    </>
     <div className="landing-page-container">
       <div className="content-wrap">
         <Header />
-        <ProductList />
-        <span>Landing Page</span>
+        <Newsletter />
       </div>
       <Footer />
     </div>
