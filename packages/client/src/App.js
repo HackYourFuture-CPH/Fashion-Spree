d707f5d641f8de6baf2d7ff9f83e4f49fb53dd6f
HackYourFutureDK <karolina.urnieziute@gmail.com>
2022-11-27 16:02:38 +0200

    fix(App): separate SearchPage

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
 import { ProductView } from './containers/ProductView/ProductView.container';
-
+import ProductList from './components/ProductList/ProductList.component';
+// TODO: change ProductList into the page container CLASS22-40
 function App() {
   return (
     <div className="app">
@@ -12,7 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path="/collections" element={<ProductList />} />
           <Route path="/products/:id" element={<ProductView />} />
+          {/* TODO: change ProductList into the page container CLASS22-40 */}
         </Routes>
       </Router>
     </div>
--- packages/client/src/containers/LandingPage/LandingPage.Container.js
+++ packages/client/src/containers/LandingPage/LandingPage.Container.js
@@ -2,15 +2,12 @@ import React from 'react';
 import './LandingPage.Style.css';
 import { Header } from '../../components/Header/Header.component';
 import Footer from '../../components/Footer/Footer';
-import ProductList from '../../components/ProductList/ProductList.component';
 
 export const LandingPage = () => {
   return (
     <div className="landing-page-container">
       <div className="content-wrap">
         <Header />
-        <ProductList />
-        <span>Landing Page</span>
       </div>
       <Footer />
     </div>
