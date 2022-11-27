2e199210698ca5ccc3b9994fb2910aec6d563cb8
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-11-27 12:25:36 +0100

    feature(ProductView): Add product view route

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { HomePage } from './containers/HomePage/HomePage.Container';
+import { LandingPage } from './containers/LandingPage/LandingPage.Container';
 import { PageNotFound } from './containers/PageNotFound/PageNotFound.Container';
+import { ProductView } from './containers/ProductView/ProductView.container';
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="*" element={<PageNotFound />} />
+          <Route path="/products/:id" element={<ProductView />} />
         </Routes>
       </Router>
     </div>
