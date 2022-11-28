cc0e3fb29eb9ce2a6caf879299813a624a8ec605
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-11-28 15:21:50 +0100

    fix (Add all components): adding components on homepage

--- packages/client/src/components/Footer/Footer.component.js
+++ packages/client/src/components/Footer/Footer.component.js
@@ -1,5 +1,116 @@
 import React from 'react';
+import './Footer.styles.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faInstagramSquare,
+  faFacebook,
+  faSquareTwitter,
+  faTiktok,
+} from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
-export const Footer = () => {
-  return <>Footer</>;
-};
+export default function Footer() {
+  return (
+    <div className="footer-container-main">
+      <div className="footer">
+        <div className="main-footer">
+          <div className="footer-container">
+            {/* Column1 */}
+            <div className="row">
+              <div className="col">
+                <h4>About Us</h4>
+                <p className="list-footer">
+                  <li>
+                    <Link to="/company">Company</Link>
+                  </li>
+                  <li>
+                    <Link to="/partners">Partners</Link>
+                  </li>
+                  <li>
+                    <Link to="/press">Press</Link>
+                  </li>
+                  <li>
+                    <Link to="/careers">Careers</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                </p>
+              </div>
+              {/* Column2 */}
+              <div className="col">
+                <h4>Customer care</h4>
+                <p className="list-footer">
+                  <li>
+                    <Link to="/shipping-delivery">Shipping and Delivery</Link>
+                  </li>
+                  <li>
+                    <Link to="/return-refunds">Return and refunds</Link>
+                  </li>
+                  <li>
+                    <Link to="/customer-service">Customer Service</Link>
+                  </li>
+                  <li>
+                    <Link to="/store-locator">Store Locator</Link>
+                  </li>
+                  <li>
+                    <Link to="/size-guide">Size Guide</Link>
+                  </li>
+                </p>
+              </div>
+              {/* Column3 */}
+              <div className="col">
+                <h4>Information</h4>
+                <p className="list-footer">
+                  <li>
+                    <Link to="/terms-service">Terms of Service</Link>
+                  </li>
+                  <li>
+                    <Link to="/privacy-policies">Privacy policies</Link>
+                  </li>
+                  <li>
+                    <Link to="/cookies-policies">Cookies policies</Link>
+                  </li>
+                  <li>
+                    <Link to="/contactsUs">Contact us</Link>
+                  </li>
+                  <li>
+                    <Link to="/FAQs">FAQs</Link>
+                  </li>
+                </p>
+              </div>
+              {/* Column4 */}
+              <div className="col">
+                <h4>Social Media</h4>
+                <p className="list-footer">
+                  <li>
+                    <FontAwesomeIcon icon={faInstagramSquare} size="lg" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faFacebook} size="lg" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faGlobe} size="lg" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faSquareTwitter} size="lg" />
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faTiktok} size="lg" />
+                  </li>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* footer bottom */}
+        <div className="main-sm-footer">
+          <p>
+            ⓒ {new Date().getFullYear()}. Fashion spree, All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- packages/client/src/containers/HomePage/HomePage.Container.js
+++ packages/client/src/containers/HomePage/HomePage.Container.js
@@ -1,48 +1,33 @@
 import React from 'react';
-import './HomePage.Style.css';
+import './HomePage.styles.css';
 import { Navigation } from '../../components/Navigation/Navigation.component';
 import { Header } from '../../components/Header/Header.component';
 import { Body } from '../../components/Body/Body.component';
-import { Newsletter } from '../../components/Newsletter/Newsletter.component';
-import { Footer } from '../../components/Footer/Footer.component';
+import Newsletter from '../../components/Newsletter/Newsletter.component';
+import Footer from '../../components/Footer/Footer.component';
+import { Layout } from '../../components/shared/Layout/Layout.component';
 
 export const HomePage = () => {
   return (
     <div className="homepage-container">
-      <div className="navigation-container">
-        <div className="navigation">
-          <Navigation />
-        </div>
-      </div>
+      <Navigation />
       <div className="header-container">
         <div className="navigation">
           <Header />
         </div>
       </div>
       <div className="divider-container"> </div>
-      <div className="body-container">
-        <div className="body">
-          <Body />
-        </div>
-      </div>
+      <Layout>
+        <Body />
+      </Layout>
       <div className="divider-container"> </div>
-      <div className="newsletter-container">
+      <div className="newsletter-container-main">
         <div className="newsletter">
           <Newsletter />
         </div>
       </div>
       <div className="divider-container"> </div>
-      <div className="footer-container">
-        <div className="footer">
-          <Footer />
-        </div>
-      </div>
-
-      {/*
-      exampleResources.map((example) => (
-        <div key={example.id}>{example.title}</div>
-      ))
-      */}
+      <Footer />
     </div>
   );
 };
