e170ebb413b53064f49daf0a576d0b72ff5ae040
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-11-29 22:28:52 +0100

    feature(search input): create shearch input

---
+++ packages/client/src/components/SearchInput/SearchInput.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchInput.style.css';
+
+const SearchInput = () => {
+  return (
+    <div className="search-container">
+      <input placeholder="Search" className="search-input" />
+    </div>
+  );
+};
+
+export default SearchInput;
--- packages/client/src/containers/CollectionsPage/CollectionsPage.Container.js
+++ packages/client/src/containers/CollectionsPage/CollectionsPage.Container.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductList from '../../components/ProductList/ProductList.component';
 import ProductListDropDown from '../ProductListDropDown/ProductListDropDown.container';
+import SearchInput from '../../components/SearchInput/SearchInput.component';
 
 export const CollectionsPage = () => {
   return (
     <div>
       <ProductListDropDown />
+      <SearchInput />
       <ProductList />
     </div>
   );
