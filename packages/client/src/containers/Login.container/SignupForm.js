573f8aad4e1187fa19ff04a7a9c9ce2c622c6f58
Hailegebriel22 <eartalea@gmail.com>
2022-12-02 23:24:16 +0100

    fix yarn lint

--- packages/client/src/containers/Login.container/SignupForm.js
+++ packages/client/src/containers/Login.container/SignupForm.js
@@ -3,61 +3,61 @@ import './Login.css';
 import FormValidation from './FormValidation';
 
 const SignupForm = () => {
-    const [formValues, setFormValues] = useState({
-        fullname: '',
-        email: '',
-        password: '',
-    });
-    const [formErrors, setFormErrors] = useState({});
-    const handleChange = (e) => {
-        setFormValues({ ...formValues, [e.target.name]: e.target.value });
-    };
+  const [formValues, setFormValues] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+  });
+  const [formErrors, setFormErrors] = useState({});
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setFormErrors(FormValidation(formValues));
-    };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormErrors(FormValidation(formValues));
+  };
 
-    return (
-        <form className="signup-form">
-            <input
-                className="sign-up-form__fullname"
-                placeholder="Full Name"
-                name="fullname"
-                values={formValues.fullname}
-                onChange={handleChange}
-                type="text"
-            />
-            {formErrors.fullname && <p>{formErrors.fullname}</p>}
-            <input
-                className="sign-up-form__email"
-                placeholder="Email Address"
-                name="email"
-                type="email"
-                values={formValues.email}
-                onChange={handleChange}
-            />
-            {formErrors.email && <p>{formErrors.email}</p>}
+  return (
+    <form className="signup-form">
+      <input
+        className="sign-up-form__fullname"
+        placeholder="Full Name"
+        name="fullname"
+        values={formValues.fullname}
+        onChange={handleChange}
+        type="text"
+      />
+      {formErrors.fullname && <p>{formErrors.fullname}</p>}
+      <input
+        className="sign-up-form__email"
+        placeholder="Email Address"
+        name="email"
+        type="email"
+        values={formValues.email}
+        onChange={handleChange}
+      />
+      {formErrors.email && <p>{formErrors.email}</p>}
 
-            <input
-                className="sign-up-form__password"
-                placeholder="Password"
-                name="password"
-                type="password"
-                values={formValues.password}
-                onChange={handleChange}
-            />
-            {formErrors.password && <p>{formErrors.password}</p>}
+      <input
+        className="sign-up-form__password"
+        placeholder="Password"
+        name="password"
+        type="password"
+        values={formValues.password}
+        onChange={handleChange}
+      />
+      {formErrors.password && <p>{formErrors.password}</p>}
 
-            <button
-                onClick={handleSubmit}
-                className="signup-submit-button"
-                type="submit"
-            >
-                Sign up
-            </button>
-        </form>
-    );
+      <button
+        onClick={handleSubmit}
+        className="signup-submit-button"
+        type="submit"
+      >
+        Sign up
+      </button>
+    </form>
+  );
 };
 
 export default SignupForm;
