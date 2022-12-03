981a9696109596819df8fc9a760ba0445843c081
Hailegebriel22 <eartalea@gmail.com>
2022-12-03 10:22:30 +0100

    continue as a guest text

--- packages/client/src/containers/Login.container/Login.js
+++ packages/client/src/containers/Login.container/Login.js
@@ -32,8 +32,10 @@ export const Login = () => {
         </div>
         <p className="or-text">-OR-</p>
         <div>
-          <SignupForm /> ;
+          <SignupForm /> 
         </div>
+        <span className='dont-want-signup'> Dont want to Sign Up ? Continue as a Guest</span>
+
       </div>
     </main>
   );
