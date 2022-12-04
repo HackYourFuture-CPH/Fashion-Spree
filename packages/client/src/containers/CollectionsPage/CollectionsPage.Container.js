6d38fdd92ea8312f4f389df7ac9d9c0c9938d088
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-12-04 16:34:43 +0100

    feature(SearchInput): add input and css

--- packages/client/src/containers/CollectionsPage/CollectionsPage.Container.js
+++ packages/client/src/containers/CollectionsPage/CollectionsPage.Container.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProductList from '../../components/ProductList/ProductList.component';
 import DropDownViewList from '../../components/ProductListDropDown/DropDownViewList/DropDownViewList.component';
+import SearchInput from '../../components/SearchInput/SearchInput.component';
 
 export const CollectionsPage = () => {
   return (
     <div>
       <DropDownViewList />
+      <SearchInput />
       <ProductList />
     </div>
   );
