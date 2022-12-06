7332e06ce235bf1e1bcb6134744bf06f81539bad
viedamayanti <vinaniekdamayanti@gmail.com>
2022-12-06 19:19:12 +0100

    migrations review and seeds

--- packages/server/migrations/20221205174532_reviews-table.js
+++ packages/server/migrations/20221205174532_reviews-table.js
@@ -4,12 +4,13 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable('reviews', (table) => {
-    table.increments('id').notNullable();
-    table.integer('product_id').notNullable();
-    table.integer('user_id').notNullable();
+    table.increments();
+    table.integer('product_id').unsigned();
+    table.foreign('product_id').references('id').inTable('products');
+    table.integer('user_id').unsigned();
+    table.foreign('user_id').references('id').inTable('users');
     table.enum('rating', ['1', '2', '3', '4', '5']);
     table.string('review_text');
-    table.foreign('reviews_id').references('id').inTable('products');
   });
 };
 
