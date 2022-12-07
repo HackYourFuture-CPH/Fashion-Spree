e0d2cc3242c20c426a68ce23b8a3cf59b28dffe1
Hanouj <hannaremy@outlook.com>
2022-12-07 13:44:09 +0100

    feat(favorites): add more seeds

--- packages/server/seeds/development/favorites.js
+++ packages/server/seeds/development/favorites.js
@@ -9,5 +9,14 @@ exports.seed = async function (knex) {
     { user_id: '1', product_id: '1' },
     { user_id: '2', product_id: '2' },
     { user_id: '3', product_id: '1' },
+    { user_id: '4', product_id: '6' },
+    { user_id: '5', product_id: '2' },
+    { user_id: '6', product_id: '5' },
+    { user_id: '7', product_id: '1' },
+    { user_id: '8', product_id: '4' },
+    { user_id: '9', product_id: '9' },
+    { user_id: '10', product_id: '1' },
+    { user_id: '11', product_id: '2' },
+    { user_id: '12', product_id: '10' },
   ]);
 };
