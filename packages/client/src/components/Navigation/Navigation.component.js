8ef6330ea30eaff0d3bb99bdb9bc741ae434b234
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2022-12-08 00:36:24 +0100

    fix(prettier)

--- packages/client/src/components/Navigation/Navigation.component.js
+++ packages/client/src/components/Navigation/Navigation.component.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { useUser } from '../../userContext';
-
 import './Navigation.styles.css';
 import { Button } from '../Button/Button.component';
 
