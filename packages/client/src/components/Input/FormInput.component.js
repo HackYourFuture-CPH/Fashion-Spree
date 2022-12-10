a4c509b400a4b43748f0368a1715b8d22753e690
Hailegebriel22 <eartalea@gmail.com>
2022-12-10 12:22:04 +0100

    feat(FormInput component)

---
+++ packages/client/src/components/Input/FormInput.component.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import validateForm from '../../utils/validateForm';
+import './FormInput.style.css';
+
+const FormInput = ({ value, type, name, label, placeholder, onChange }) => {
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setErrors(validateForm(event.target.value));
+    onChange(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      {label && <label>{label}</label>}
+
+      <input
+        className="form-input"
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+      {errors && <p className="error-message">{errors.message}</p>}
+    </div>
+  );
+};
+
+FormInput.propTypes = {
+  value: PropTypes.string,
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.oneOf(['text', 'email', 'password']),
+  onChange: PropTypes.func.isRequired,
+};
+
+FormInput.defaultProps = {
+  value: '',
+  name: '',
+  label: '',
+  placeholder: '',
+  type: 'text',
+};
+
+export default FormInput;
