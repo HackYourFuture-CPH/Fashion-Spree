48b6180d5d525a0c72ef81887a7b47cc9b148987
shravanipagilla <nampally04@gmail.com>
2022-12-11 15:06:02 +0100

    bug fix

--- packages/client/src/components/ViewDropDown/ViewDropdown.component.js
+++ packages/client/src/components/ViewDropDown/ViewDropdown.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes, { string } from 'prop-types';
+import PropTypes from 'prop-types';
 import './ViewDropdown.styles.css';
 
 const ViewDropdown = ({ options, label, ...props }) => {
