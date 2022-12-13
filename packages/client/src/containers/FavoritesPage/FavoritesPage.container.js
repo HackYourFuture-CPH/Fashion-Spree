bca781de219c9bb67a10952f2df618e9fdace14e
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-12-13 13:27:07 +0100

    feature(FavoritesPage): add functionality to Favoritas page

--- packages/client/src/containers/FavoritesPage/FavoritesPage.container.js
+++ packages/client/src/containers/FavoritesPage/FavoritesPage.container.js
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchInput from '../../components/SearchInput/SearchInput.component';
 import { getLocalStorage, setLocalStorage } from '../../utils/storageHelpers';
 import ProductCard from '../../components/ProductCard/ProductCard.component';
+import './FavoritesPage.style.css';
 
 export const FavoritesPage = () => {
   const favoriteProductsStorageKey = 'favorite_products';
-  const storageItems = getLocalStorage(favoriteProductsStorageKey);
 
-  const ListOfFavoriteProducts = storageItems.map((product) => {
+  const [favoriteProducts, setFavoriteProducts] = useState(
+    getLocalStorage(favoriteProductsStorageKey) || [],
+  );
+  const [isFavorite, setIsFavorite] = useState(true);
+
+  const toggleFavorite = (id, title, price, event) => {
+    const newFavoriteItem = {
+      id,
+      title,
+      price,
+    };
+
+    const filteredFavorite = favoriteProducts.filter((item) => item.id !== id);
+    setFavoriteProducts(filteredFavorite);
+    setLocalStorage(favoriteProductsStorageKey, filteredFavorite);
+  };
+
+  const ListOfFavoriteProducts = favoriteProducts.map((product) => {
     return (
       <div className="product">
         <ProductCard
           key={product.id}
-          title={product.name}
+          title={product.title}
           price={product.price}
           id={product.id}
+          toggleFavorite={toggleFavorite}
+          isFavorite={favoriteProducts.some((x) => x.id === product.id)}
         />
       </div>
     );
   });
 
   return (
-    <>
+    <div className="list-view">
       <SearchInput />
-      <div>{ListOfFavoriteProducts}</div>
-    </>
+      <h2 className="my-favorites">My favorites</h2>
+      <div className="rendered-product">{ListOfFavoriteProducts}</div>
+    </div>
   );
 };
