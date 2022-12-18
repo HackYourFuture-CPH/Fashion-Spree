18c5c473e26faedd99d7c09c0db3cfe77f62f5ef
Hanouj <hannaremy@outlook.com>
2022-12-18 15:28:28 +0100

    feat(Hamburger-menu): add responsive hamburger navigation

--- packages/client/src/components/Navigation/Navigation.component.js
+++ packages/client/src/components/Navigation/Navigation.component.js
@@ -1,53 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { useUserContext } from '../../userContext';
 import './Navigation.styles.css';
 import { Button } from '../Button/Button.component';
+import { FaBars } from 'react-icons/fa';
+import { ImCross } from 'react-icons/im';
 
 export const Navigation = () => {
   const { user, name, logout } = useUserContext();
+  const [showLinks, setShowLinks] = useState(false);
 
   return (
     <div className="navigation-container">
       <div className="navigation">
         <div className="nav">
           <span className="navbar-brand">Fashion Spree</span>
-          <NavLink to="/" className="home">
-            Home
-          </NavLink>
-          <NavLink to="/about-us" className="about-us">
-            About Us
-          </NavLink>
-          <NavLink to="/collections" className="collections">
-            Our collections
-          </NavLink>
-          <NavLink to="/contact-us" className="contact-us">
-            Contact Us
-          </NavLink>
-          {user ? (
-            <div className="logged-in-container">
-              <div className="logged-in-text">
-                <div>{name}</div>
-                <div>{user.email}</div>
+          <div className="links" id={showLinks ? 'links-mobile' : ''}>
+            <NavLink to="/" className="home">
+              Home
+            </NavLink>
+            <NavLink to="/about-us" className="about-us">
+              About Us
+            </NavLink>
+            <NavLink to="/collections" className="collections">
+              Our collections
+            </NavLink>
+            <NavLink to="/contact-us" className="contact-us">
+              Contact Us
+            </NavLink>
+            {user ? (
+              <div className="logged-in-container">
+                <div className="logged-in-text">
+                  <div>{name}</div>
+                  <div>{user.email}</div>
+                </div>
+                <div className="logout-button-container">
+                  <Button
+                    label="Logout"
+                    backgroundColor="#F5F5F5"
+                    onClick={logout}
+                  />
+                </div>
               </div>
-              <div className="logout-button-container">
-                <Button
-                  label="Logout"
-                  backgroundColor="#F5F5F5"
-                  onClick={logout}
-                />
+            ) : (
+              <div className="nav-buttons">
+                <Link to="/login" className="login">
+                  <Button label="Login" backgroundColor="#F5F5F5" />
+                </Link>
+                <Link to="/signup" className="signup">
+                  <Button label="Sign Up" backgroundColor="#F5F5F5" />
+                </Link>
               </div>
-            </div>
-          ) : (
-            <div className="nav-buttons">
-              <Link to="/login" className="login">
-                <Button label="Login" backgroundColor="#F5F5F5" />
-              </Link>
-              <Link to="/signup" className="signup">
-                <Button label="Sign Up" backgroundColor="#F5F5F5" />
-              </Link>
-            </div>
-          )}
+            )}
+          </div>
+          <button
+            onClick={() => setShowLinks(!showLinks)}
+            type="submit"
+            className="mobile-menu-icon"
+          >
+            {showLinks ? <ImCross /> : <FaBars />}
+          </button>
         </div>
       </div>
     </div>
