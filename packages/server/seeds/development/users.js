4194ce5cb5fd217f2d5753e86c5a633e0cc3a1e1
Yuliia-Balandiuk <73549460+Yuliia-Balandiuk@users.noreply.github.com>
2022-12-22 11:33:18 +0100

    feature(DataBase): create users endpoint and edit users seed

--- packages/server/seeds/development/users.js
+++ packages/server/seeds/development/users.js
@@ -6,7 +6,11 @@ exports.seed = async function (knex) {
   await knex('users').del();
   await knex('users').insert([
     { full_name: 'Ada Lovelace', email: 'ada@lovelace.com' },
-    { full_name: 'Sally Ride', email: 'sally@ride.com' },
+    {
+      full_name: 'Yuliia Balandiuk',
+      email: 'yuliia.balandiuk8@gmail.com',
+      uid: 'TfF0SrbO1MeFTdYa4wzv',
+    },
     { full_name: 'Alan Turing', email: 'alan@turing.com' },
     { full_name: 'Steve Jobs', email: 'steve@jobs.com' },
   ]);
