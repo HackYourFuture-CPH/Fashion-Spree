df9e44496809a516d61d676a099bbe6032d4f3da
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2023-01-10 22:56:38 +0100

    fix

--- packages/client/src/containers/Signup/Signup.container.js
+++ packages/client/src/containers/Signup/Signup.container.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUserContext } from '../../userContext';
 import './Signup.styles.css';
